5714f583e57d9e2df04caea151a4b298b3c61af1
rednepal <jvprimer@gmail.com>
2015-04-29 10:42:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello New World forked!! another");
+        System.out.println("Hello New World forked!! another 2");
     }
 }
